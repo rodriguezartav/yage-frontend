68ad67a35c471fca9aafdbe102a02f1ff411e0f3
Roberto <roberto@rodriguezartavia.com>
2017-12-10 09:38:46 -0600

    build

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        API_URL: process.env.NODE_ENV == "staging" ? JSON.stringify("http://xxxxx.com") : JSON.stringify("https://xxxxxx.com")
+        API_URL: process.env.NODE_ENV == "staging" ? JSON.stringify("http://apistaging.ceremoniacusingas.org") : JSON.stringify("https://api.ceremoniacusingas.org")
       }
     }),
     new CleanWebpackPlugin(["dist/*.js","dist/*.css","dist/*.html","dist/*.map","dist/*.gz"], {verbose: true}),
