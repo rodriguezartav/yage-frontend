22af311235456cc7c2b5cf108e0b59ecdfc5d515
Roberto <roberto@rodriguezartavia.com>
2018-01-04 10:29:39 -0600

    update pagos

--- ui/metadata/index.js
+++ ui/metadata/index.js
@@ -1,10 +1,11 @@
 import pacientes from "./pacientes.json"
 import reservaciones from "./reservaciones.json"
-
+import pagos from "./pagos.json"
 
 function Metadata(name){
   if( name == "pacientes" ) return pacientes;
   else if(name=="reservaciones") return reservaciones;
+  else if(name=="pagos") return pagos;
 }
 
 export default Metadata
