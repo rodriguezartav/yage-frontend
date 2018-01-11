43a8f37ccc3acb4e9ff79b56229e4d5d4066a5ab
Roberto <roberto@rodriguezartavia.com>
2018-01-11 17:19:50 -0600

    error boundery and ie fix outside

--- ui/apps/reservaciones/index.jsx
+++ ui/apps/reservaciones/index.jsx
@@ -8,9 +8,9 @@ import { AppContainer } from 'react-hot-loader';
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <ErrorBoundary>
+
         <App {...window.__APP_INITIAL_STATE__} />
-      </ErrorBoundary>
+
     </AppContainer>
 
    , document.getElementById('root')
@@ -23,27 +23,3 @@ if (module.hot) module.hot.accept('./container', () => {
   console.log("hot")
   render(App)
 });
-
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  componentDidCatch(error, info) {
-    // Display fallback UI
-    this.setState({ hasError: true });
-    // You can also log the error to an error reporting service
-    //logErrorToMyService(error, info);
-    trackJs.track(error);
-
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
-    }
-    return this.props.children;
-  }
-}
