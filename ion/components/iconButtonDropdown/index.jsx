4d1a9c94b58db4f879a77f78655452dc386c383a
Roberto <roberto@rodriguezartavia.com>
2018-01-31 21:23:44 -0600

    update rr

--- ion/components/iconButtonDropdown/index.jsx
+++ ion/components/iconButtonDropdown/index.jsx
@@ -29,13 +29,13 @@ class IconButtonWithDropdown extends React.Component {
     var _this = this;
     return <div onClick={_this.onClick.bind(_this)} key="help"
     className={classnames("slds-m-left_xx-small","slds-dropdown-trigger","slds-dropdown-trigger_click",{"slds-is-open":this.state.isOpen})}>
-    <button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true" title="Show More">
-    <svg class="slds-button__icon" aria-hidden="true">
+    <button className="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true" title="Show More">
+    <svg className="slds-button__icon" aria-hidden="true">
       <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#question"></use>
     </svg>
   </button>
-  <div class="slds-dropdown slds-dropdown_left slds-dropdown_small">
-    <ul class="slds-dropdown__list" role="menu">
+  <div className="slds-dropdown slds-dropdown_left slds-dropdown_small">
+    <ul className="slds-dropdown__list" role="menu">
     { (_this.props.items || []).map(function(item){
       return <li key={item[_this.props.field]} className="slds-dropdown__item" role="presentation">
       <a role="menuitem" tabindex="0">
